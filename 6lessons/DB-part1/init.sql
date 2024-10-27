b5f9abee3045e081bd86ac2369f78da09f0a6f68
Kostia Stoliarskiy <saintkos117@gmail.com>
2024-10-27 10:26:14 +0200

    cascade delete

--- 6lessons/DB-part1/init.sql
+++ 6lessons/DB-part1/init.sql
@@ -1,18 +1,10 @@
 CREATE TABLE IF NOT EXISTS Users (
     ID SERIAL PRIMARY KEY,
-    Name VARCHAR(100),
-    Email VARCHAR(100)
+    Name VARCHAR(100) NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS UserPreferences (
-    PreferenceID SERIAL PRIMARY KEY,
-    LanguageCode VARCHAR(100),
-    PreferredCommunication INT,
+    id SERIAL PRIMARY KEY,
     UserId INT,
-    FOREIGN KEY (UserId) REFERENCES Users(ID)
+    description VARCHAR(255),
+    FOREIGN KEY (UserId) REFERENCES Users(id) ON DELETE CASCADE
 );
-
-
-
--- ALTER TABLE users ADD UNIQUE (name)
--- ALTER TABLE users ALTER COLUMN name SET NOT NULL;
